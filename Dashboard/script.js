c57efcc4556ae7b29d9f2e21c4c7a4e481d16514
OtavioRodrrigues365 <otavio.araujo@sptech.school>
2025-04-21 21:46:05 -0300

    Tela da dashboard

---
+++ Dashboard/script.js
@@ -0,0 +1,116 @@
+const grafico1 = document.getElementById('grafico-line');
+
+new Chart(grafico1, {
+    type: 'line',
+    data: {
+        labels: ['00:00', '04:00', '08:00', '12:00', '16:00', '20:00'],
+        datasets: [{
+            label: 'Umidade Máxima',
+            backgroundColor: ' #FF0004',
+            borderColor: ' #FF0004',
+            data: [80, 80, 80, 80, 80, 80],
+            borderWidth: 1,
+            pointStyle: false
+        },
+        {
+            label: 'Umidade',
+            backgroundColor: ' #6D54EE',
+            borderColor: ' #6D54EE',
+            data: [70, 47, 66, 75, 62, 65],
+            borderWidth: 2,
+            pointStyle: false
+        },
+        {
+            label: 'Umidade Mínima',
+            backgroundColor: ' #F8A720',
+            borderColor: ' #F8A720',
+            data: [60, 60, 60, 60, 60, 60],
+            borderWidth: 2,
+            pointStyle: false
+        }]
+    },
+    options: {
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                min: 0,
+                max: 100,
+                beginAtZero: true,
+                ticks: {
+                    stepSize: 20
+                  }
+            },
+            
+        }
+    }
+});
+
+const grafico2 = document.getElementById('grafico-pizza');
+
+new Chart(grafico2, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            label: ['Não Funcionando',
+                'Funcionando'],
+            backgroundColor: [' #FF9F00',
+                ' #6D54EE'],
+            data: [20, 200],
+            borderWidth: 1,
+            pointStyle: false
+        },]
+    },
+    options: {
+        
+    }
+});
+
+const grafico3 = document.getElementById('grafico-line2');
+
+new Chart(grafico3, {
+    type: 'bar',
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        datasets: [{
+            type: 'line',
+            label: 'Umidade Máxima',
+            backgroundColor: ' #FF0004',
+            borderColor: ' #FF0004',
+            data: [80, 80, 80, 80, 80, 80,80,80,80,80,80,80],
+            borderWidth: 1,
+            pointStyle: false
+        },
+        {
+            type: 'line',
+            label: 'Umidade Mínima',
+            backgroundColor: ' #F8A720',
+            borderColor: ' #F8A720',
+            data: [60, 60, 60, 60, 60, 60,60,60,60,60,60,60],
+            borderWidth: 2,
+            pointStyle: false
+        },
+        {
+            label: 'Umidade',
+            backgroundColor: ' #6D54EE',
+            borderColor: ' #6D54EE',
+            data: [24,55,30,45,65,55,26,40,85,40,10,55],
+            borderWidth: 2,
+            pointStyle: false
+        }
+        ]
+    },
+    options: {
+        maintainAspectRatio: false,
+        scales: {
+            y: {
+                min: 0,
+                max: 100,
+                beginAtZero: true,
+                ticks: {
+                    stepSize: 20
+                  }
+            },
+            
+        }
+    }
+});
