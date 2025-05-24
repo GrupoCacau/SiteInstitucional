7b2513fad9fc0095360951f65d3bc83e4d60e58e
Gui-DoSantos <guilherme.santos061@sptech.school>
2025-05-24 16:05:17 -0300

    adicionando o bobIA no site institucional

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -1,11 +1,14 @@
 // var ambiente_processo = 'producao';
 var ambiente_processo = 'desenvolvimento';
 
+
 var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 // Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
 // A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
 
 require("dotenv").config({ path: caminho_env });
+// importando os bibliotecas necessárias
+const { GoogleGenAI } = require("@google/genai");
 
 var express = require("express");
 var cors = require("cors");
@@ -45,3 +48,53 @@ app.listen(PORTA_APP, function () {
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
     \t\tPara alterar o ambiente, comente ou descomente as linhas 1 ou 2 no arquivo 'app.js'\n\n`);
 });
+
+
+// configurando o servidor express
+const PORTA_SERVIDOR = process.env.PORTA;
+
+// configurando o gemini (IA)
+const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE });
+
+// configurando o servidor para receber requisições JSON
+app.use(express.json());
+
+// configurando o servidor para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// rota para receber perguntas e gerar respostas
+app.post("/perguntar", async (req, res) => {
+    const pergunta = req.body.pergunta;
+
+    try {
+        const resultado = await gerarResposta(pergunta);
+        res.json({ resultado });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+
+});
+
+// função para gerar respostas usando o gemini
+async function gerarResposta(mensagem) {
+
+    try {
+        // gerando conteúdo com base na pergunta
+        const modeloIA = chatIA.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: `Em um paragráfo responda: ${mensagem}`
+
+        });
+        const resposta = (await modeloIA).text;
+        const tokens = (await modeloIA).usageMetadata;
+
+        console.log(resposta);
+        console.log("Uso de Tokens:", tokens);
+
+        return resposta;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
